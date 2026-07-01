1c2bb94c6cab13298243a078d92062a115e3b250
Bruce Zhang <zttt183525594@gmail.com>
2026-07-01 10:52:21 +0800

    ci: cache python package downloads

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -49,9 +49,10 @@ chaquopy {
             install("https://files.pythonhosted.org/packages/1c/70/cf356c9096d401ad63acbe686b51f1d50d491e9afb478e704c574ab17606/fuo_netease-1.0.8.tar.gz")
             install("fuo-qqmusic==1.0.16")
             install("beautifulsoup4")
-            install("marshmallow")
+            install("marshmallow==3.26.2")
             install("mutagen")
-            install("pycryptodome")
+            install("pydantic==1.10.26")
+            install("pycryptodome==3.21.0")
             install("requests")
         }
     }
